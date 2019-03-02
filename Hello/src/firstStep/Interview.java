3023392493a4e2c855b700a760cf27682e7dc4c9
josonlee <2559820564@qq.com>
2019-03-02 23:16:20 +0800

    增加java核心卷第五章内容

--- Hello/src/firstStep/Interview.java
+++ Hello/src/firstStep/Interview.java
@@ -1,14 +1,15 @@
 package firstStep;
 
 public class Interview {
-	private static String nameString;
+	private static String nameString = "lzw";
+	public static String nameTest = "lzw";
 	private static int age = 20;
 	private double toDay;
 	public final static String DATE = "2020";
 	
 	public Interview(String name,double day) {
 		// TODO Auto-generated constructor stub
-		this.nameString = name;
+		Interview.nameTest = name;
 		this.toDay = day;
 	}
 	public static void print_static() {
@@ -16,6 +17,6 @@ public class Interview {
 	}
 	public void print() {
 		System.out.println(nameString+" "+age+" "+toDay);
-		System.out.println(DATE);
+		System.out.println(this.getClass().getSimpleName()+":"+DATE+" "+nameTest);
 	}
 }
--- Hello/src/test/Utils.java
+++ Hello/src/test/Utils.java
@@ -6,9 +6,12 @@ public class Utils {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//The static field Interview.nameString should be accessed in a static way
+		System.out.println(Interview.nameTest);
 		Interview day1 = new Interview("josonlee", 0.99);
 		day1.print();
-		Interview.print_static();
+		Interview.nameTest = "hadoop";
+		System.out.println(Interview.nameTest);
+//		Interview.print_static();
 	}
 
 }
