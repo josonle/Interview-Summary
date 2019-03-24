98fd230933a1b337b1784b2716fc10284258254b
josonlee <2559820564@qq.com>
2019-03-24 21:54:00 +0800

    补充

--- Hello/src/firstStep/SynchronizedExample.java
+++ Hello/src/firstStep/SynchronizedExample.java
@@ -1,5 +1,10 @@
 package firstStep;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 
 public class SynchronizedExample{
@@ -15,6 +20,18 @@ public class SynchronizedExample{
 		SynchronizedExample e = new SynchronizedExample();
 		es.execute(()->e.fun());
 		es.execute(()->e.fun());
-		es.execute(()->e.fun());
+		
+		Lock lock = new ReentrantLock();
+		Condition condition = lock.newCondition();
+		try {
+			condition.await();
+			condition.signal();
+			condition.signalAll();
+		} catch (Exception e2) {
+			// TODO: handle exception
+		}
+//		CountDownLatch
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
+		
 	}
 }
